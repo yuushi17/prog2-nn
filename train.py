7d7ec0cf42219ecd752b46ade5668b0b348e3f82
Yuushi Yamamoto <j4351tdx@sit.ac.jp>
2025-12-09 15:54:32 +0900

    時間の桁数を小数点以下3桁にする

--- train.py
+++ train.py
@@ -63,7 +63,7 @@ for epoch in range(n_epochs):
     time_start = time.time()
     train_loss = models.train(model, dataloader_train, loss_fn, optimizer)
     time_end = time.time()
-    print(f'    training loss: {train_loss} ({time_end-time_start}s)')
+    print(f'    training loss: {train_loss} ({time_end-time_start:.3f}s)')
     train_loss_log.append(train_loss)
 
     val_loss = models.test(model, dataloader_test, loss_fn)
