3235e5a766f19a6cbfc5b55874bb1bd965e7b6bb
Yuushi Yamamoto <j4351tdx@sit.ac.jp>
2025-12-09 15:47:24 +0900

    学習時間(1エポック)の計算

--- train.py
+++ train.py
@@ -60,8 +60,10 @@ val_acc_log = []
 for epoch in range(n_epochs):
     print(f'epoch {epoch+1}/{n_epochs}')
 
+    time_start = time.time()
     train_loss = models.train(model, dataloader_train, loss_fn, optimizer)
-    print(f'    training loss: {train_loss}')
+    time_end = time.time()
+    print(f'    training loss: {train_loss} ({time_end-time_start}s)')
     train_loss_log.append(train_loss)
 
     val_loss = models.test(model, dataloader_test, loss_fn)
